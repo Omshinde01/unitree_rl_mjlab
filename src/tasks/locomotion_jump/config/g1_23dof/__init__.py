f2f1cf1e70ab4a35a1482673c6af0222afdb880d
OM Shinde <om.shinde@kodytechno.local>
2026-08-17 14:23:23 +0530

    added new task for testing

---
+++ src/tasks/locomotion_jump/config/g1_23dof/__init__.py
@@ -0,0 +1,18 @@
+from mjlab.tasks.registry import register_mjlab_task
+
+from src.tasks.locomotion_jump.rl import VelocityOnPolicyRunner
+
+from .env_cfgs import (
+    unitree_g1_flat_env_cfg,
+    unitree_g1_rough_env_cfg,
+)
+from .rl_cfg import unitree_g1_ppo_runner_cfg
+
+
+register_mjlab_task(
+    task_id="Unitree-G1-Jump-Flat",
+    env_cfg=unitree_g1_flat_env_cfg(),
+    play_env_cfg=unitree_g1_flat_env_cfg(play=True),
+    rl_cfg=unitree_g1_ppo_runner_cfg(),
+    runner_cls=VelocityOnPolicyRunner,
+)
