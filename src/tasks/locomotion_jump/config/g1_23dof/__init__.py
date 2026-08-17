a1b2f31f285a8ff8fa6eb20bcc568e2743a47b8a
OM Shinde <om.shinde@kodytechno.local>
2026-08-17 16:27:52 +0530

    config update

--- src/tasks/locomotion_jump/config/g1_23dof/__init__.py
+++ src/tasks/locomotion_jump/config/g1_23dof/__init__.py
@@ -3,16 +3,16 @@ from mjlab.tasks.registry import register_mjlab_task
 from src.tasks.locomotion_jump.rl import VelocityOnPolicyRunner
 
 from .env_cfgs import (
-    unitree_g1_flat_env_cfg,
-    unitree_g1_rough_env_cfg,
+  unitree_g1_23dof_flat_env_cfg,
+  unitree_g1_23dof_rough_env_cfg,
 )
-from .rl_cfg import unitree_g1_ppo_runner_cfg
+from .rl_cfg import unitree_g1_23dof_ppo_runner_cfg
 
 
 register_mjlab_task(
-    task_id="Unitree-G1-Jump-Flat",
-    env_cfg=unitree_g1_flat_env_cfg(),
-    play_env_cfg=unitree_g1_flat_env_cfg(play=True),
-    rl_cfg=unitree_g1_ppo_runner_cfg(),
-    runner_cls=VelocityOnPolicyRunner,
+  task_id="Unitree-G1-23Dof-Jump-Flat",
+  env_cfg=unitree_g1_23dof_flat_env_cfg(),
+  play_env_cfg=unitree_g1_23dof_flat_env_cfg(play=True),
+  rl_cfg=unitree_g1_23dof_ppo_runner_cfg(),
+  runner_cls=VelocityOnPolicyRunner,
 )
