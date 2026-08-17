2e15809333de485209536d4ad1f666d0c2fc86aa
OM Shinde <om.shinde@kodytechno.local>
2026-08-17 16:25:25 +0530

    updated locomotion_jump

--- src/tasks/locomotion_jump/mdp/__init__.py
+++ src/tasks/locomotion_jump/mdp/__init__.py
@@ -5,4 +5,4 @@ from .curriculums import *  # noqa: F403
 from .observations import *  # noqa: F403
 from .rewards import *  # noqa: F403
 from .terminations import *  # noqa: F403
-from .velocity_command import
+from .velocity_command import *  # noqa: F403
